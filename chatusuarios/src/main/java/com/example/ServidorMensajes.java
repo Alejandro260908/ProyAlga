e6b232f8337179da913dc506a75722cfbd2e66ef
Alejandro <manuel.ale.10@hotmail.com>
2025-09-29 19:13:10 -0700

    Servidor

--- chatusuarios/src/main/java/com/example/ServidorMensajes.java
+++ chatusuarios/src/main/java/com/example/ServidorMensajes.java
@@ -1,5 +1,24 @@
 package com.example;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServidorMensajes {
-    
+    public static void main(String[] args) {
+        int puerto = 8080;
+
+        try (ServerSocket serverSocket = new ServerSocket(puerto)) {
+            System.out.println("Servidor iniciado en el puerto " + puerto);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Cliente conectado: " + socket);
+                new Thread(new ManejadorCliente(socket)).start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
