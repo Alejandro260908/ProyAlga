4cb9beb5b1f013cab36a92baf151307ef74b41e9
Alejandro <manuel.ale.10@hotmail.com>
2025-11-19 01:57:10 -0700

    SM 0.4 creacion de la clase para la base de datos

---
+++ chatusuarios/src/main/java/com/example/serverMulti/bdd.java
@@ -0,0 +1,7 @@
+package com.example.serverMulti;
+
+public class bdd {
+    private static String BDD_URL = "jdbc:postgresql://localhost:5432/chat_db";
+    private static String BDD_USER = "postgres";
+    private static String BDD_PASSWORD = "ElPsyCongroo123";
+}
