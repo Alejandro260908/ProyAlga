9db7c5cba5d35f0bed2d03ce740ff3046b1b38bd
Alejandro <manuel.ale.10@hotmail.com>
2025-11-19 02:04:14 -0700

    SM 0.4.1 inicializador de la conexion

--- chatusuarios/src/main/java/com/example/serverMulti/bdd.java
+++ chatusuarios/src/main/java/com/example/serverMulti/bdd.java
@@ -1,7 +1,26 @@
 package com.example.serverMulti;
 
+import java.sql.Connection;
+
 public class bdd {
     private static String BDD_URL = "jdbc:postgresql://localhost:5432/chat_db";
     private static String BDD_USER = "postgres";
     private static String BDD_PASSWORD = "ElPsyCongroo123";
+    private static Connection conexion = null;
+
+    public static void inicializarConexion() {
+        try {
+            cargarConfig();
+
+            Class.forName("org.postgresql.Driver");
+
+            crearTU();
+            crearTR();
+
+            System.out.println("Conexión a la base de datos establecida.");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+            conexion = null;
+        }
+    }
 }
