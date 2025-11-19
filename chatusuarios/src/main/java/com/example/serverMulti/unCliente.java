8a38273f9ba4a74eb43f5f097ef7e8b4af22e60d
Alejandro <manuel.ale.10@hotmail.com>
2025-11-19 01:24:39 -0700

    SM 0.3.4 salida, entrada y getter del cliente

--- chatusuarios/src/main/java/com/example/serverMulti/unCliente.java
+++ chatusuarios/src/main/java/com/example/serverMulti/unCliente.java
@@ -1,5 +1,31 @@
 package com.example.serverMulti;
 
-public class unCliente {
-    
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+;
+
+public class unCliente implements Runnable {
+    final DataOutputStream salida;
+    final DataInputStream entrada;
+    final BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+    private final String usuario;
+
+    public unCliente(String usuario, Socket socket) throws IOException {
+        salida = new DataOutputStream(socket.getOutputStream());
+        entrada = new DataInputStream(socket.getInputStream());
+        this.usuario = usuario;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    @Override
+    public void run() {
+        // Lógica del cliente
+    }
 }
